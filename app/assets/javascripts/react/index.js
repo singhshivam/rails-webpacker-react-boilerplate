00820fa807c6572102e88ac66450beb7d10a3b0b
singhshivam <shivam@blogvault.net>
2018-07-30 20:16:52 +0530

    initial commit :tada:

---
+++ app/assets/javascripts/react/index.js
@@ -0,0 +1,11 @@
+console.log('hello')
+'use strict'
+import React from 'react'
+import { render } from 'react-dom'
+
+render(
+	<div>
+		Running my react app
+	</div>,
+	document.getElementById('app')
+)
